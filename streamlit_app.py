97dad3ebacaa494aec552dd50ababe3913254830
HamzaHaline <150623980+HamzaHaline@users.noreply.github.com>
2023-11-30 01:33:16 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,85 @@
-import altair as alt
-import numpy as np
-import pandas as pd
 import streamlit as st
+import pandas as pd
+import numpy as np
+import plotly.express as px
+import plotly.graph_objects as go
+import matplotlib.pyplot as plt
+import seaborn as sns
+from fonction_project import *
+import warnings
+warnings.filterwarnings('ignore')
+#%% Traitement fichier 
+info_col = [
+    'Age', 
+    'Genre', 
+    'Education', 
+    'Pays',
+    'Ethnie',
+]
 
+caract_col = [
+    'Névrotique',
+    'Extraverti',
+    'Ouvert à l\'expérience',
+    'Amical',
+    'Consciencieux',
+    'Imuplsif',
+    'En recherche de sensations'
+]
+
+personne_col = info_col + caract_col
+
+drogues_col = [
+    'Consommation d\'alcool',
+    'Consommation d\'amphétamines',
+    'Consommation d\'amyl',
+    'Consommation de benzodiazepine',
+    'Consommation de café',
+    'Consommation de cannabis',
+    'Consommation de chocolat',
+    'Consommation de cocaïne',
+    'Consommation de crack',
+    'Consommation d\'ecstasy',
+    'Consommation d\'héroïne',
+    'Consommation de ketamine',
+    'Consommation de drogues légales',
+    'Consommation de LSD',
+    'Consommation de meth',
+    'Consommation de champignons magiques',
+    'Consommation de nicotine',
+    'Consommation de Semeron',
+    'Consommation de VSA'
+]
+
+drogues_autorisées = ['Consommation d\'alcool', 'Consommation de café', 'Consommation de chocolat', 'Consommation de nicotine']
+drogues_illégales = [i for i in drogues_col if i not in drogues_autorisées]
+col = personne_col + drogues_col
+
+data=lecture_data()
+pers_data=pre_data(data,col,drogues_col,drogues_illégales)
+
+
+
+#%% Debut Page 
+
+# TITRE : 
+st.title("Data Analyse for the drug data ")
+st.markdown("Project made by \n Yahya BOUAYAD,\n Hamza HALINE\n et Joshua BORNET")
+
+if st.checkbox("Show raw data"):
+    st.subheader('Drug Raw data used')
+    st.write(data)
+    st.markdown(
+        """
+        The dataset here is made up of 31 columns, the first represents some global characteristics about the person such as his age, education and others. Then comes some psychological aspects of the person, and the last part of the dataset is about what kind of addiction the person has, from chocolate to cocaine.
+We have 1800 individuals, which gives us a good data set to study.
+
+        """
+        )
+st.markdown("We read the documentation and thus, we were able to rename the columns and modify them to make their content readable by everybody.")
+if st.checkbox("Show cleaned data"):
+    st.subheader('Cleaned data used')
+    st.write(pers_data)
+    st.markdown("Ther was no null data, so we had nothing to remove. However, we managed to remove all of the users that tried to trick the study by saying they were Semerons consumers, whereas Semeron is a fake drug. In total, they were barely ten.")
 
 
