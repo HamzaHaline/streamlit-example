7257b83603629bcd51b1255bf99f776b52b44ada
HamzaHaline <150623980+HamzaHaline@users.noreply.github.com>
2023-11-30 11:43:38 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,57 +7,6 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from fonction_project import *
 import warnings
-warnings.filterwarnings('ignore')
-#%% Traitement fichier 
-info_col = [
-    'Age', 
-    'Genre', 
-    'Education', 
-    'Pays',
-    'Ethnie',
-]
-
-caract_col = [
-    'Névrotique',
-    'Extraverti',
-    'Ouvert à l\'expérience',
-    'Amical',
-    'Consciencieux',
-    'Imuplsif',
-    'En recherche de sensations'
-]
-
-personne_col = info_col + caract_col
-
-drogues_col = [
-    'Consommation d\'alcool',
-    'Consommation d\'amphétamines',
-    'Consommation d\'amyl',
-    'Consommation de benzodiazepine',
-    'Consommation de café',
-    'Consommation de cannabis',
-    'Consommation de chocolat',
-    'Consommation de cocaïne',
-    'Consommation de crack',
-    'Consommation d\'ecstasy',
-    'Consommation d\'héroïne',
-    'Consommation de ketamine',
-    'Consommation de drogues légales',
-    'Consommation de LSD',
-    'Consommation de meth',
-    'Consommation de champignons magiques',
-    'Consommation de nicotine',
-    'Consommation de Semeron',
-    'Consommation de VSA'
-]
-
-drogues_autorisées = ['Consommation d\'alcool', 'Consommation de café', 'Consommation de chocolat', 'Consommation de nicotine']
-drogues_illégales = [i for i in drogues_col if i not in drogues_autorisées]
-col = personne_col + drogues_col
-
-data=lecture_data()
-pers_data=pre_data(data,col,drogues_col,drogues_illégales)
-
 
 
 #%% Debut Page 
